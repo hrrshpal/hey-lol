2467423481b45e916de404eaaf2a4376b2aa6ec8
harsh <hrrshpal@gmail.com>
2024-10-06 14:01:39 +0530

    played with request handlers

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req,res) => {
+app.get("/", (req,res) => {
     res.send("hey lol")
+})
+
+app.post("/", (req,res) => {
+    // add user in the db
+    res.send("user added in the database")
+})
+
+app.patch("/", (req,res) => {
+    // update user in the db
+    res.send("user updated")
+})
+
+app.delete("/", (req,res) => {
+    // remove user
+    res.send("user deleted")
 })
 
 app.listen(3000, ()=>{
